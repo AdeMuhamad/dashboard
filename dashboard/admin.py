61089c4e1144523838efbc3ac5c92628faa5e135
ademuhammad <ademuhammad@example.com>
2019-01-03 06:59:01 +0000

    #2 change site_header admin site

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import Group
 from .models import Post, User
 
 
+admin.site.site_header = 'Oapps Dashboard'
+
 class PostAdmin(admin.ModelAdmin):
 	list_display = ('user_id','title', 'subtitle', 'slug', 'content')
 	list_filter = ('user_id','created_at')
